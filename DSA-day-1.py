242368e0bd3ec815a8f5e99c6479c7f4f79a5bd5
VIVEK TAWARE <vivektaware16@gmail.com>
2026-05-04 21:54:29 +0530

    today agenda the some theory of the lecture no one

--- DSA-day-1.py
+++ DSA-day-1.py
@@ -1,1 +1,10 @@
-# Data structure and algorithms 
+# Data structure and algorithms using python 
+# lecture no - 1 
+# today's agenda 
+# 1) what is a programming language 
+# 2) high level programming vd low level programming language 
+# 3) comipler vs interpreter 
+# 4) introduction to python 
+# 5) variables / keywords / identifiers / operator / data types / 
+# 6) input and output
+

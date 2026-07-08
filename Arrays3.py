1b8f48120ddbd186bd3a0f9db2809e597e6a0f96
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-08 20:47:29 +0530

    left rotate an array by one position

---
+++ Arrays3.py
@@ -0,0 +1,36 @@
+"""
+Left Rotate an Array by One Position
+
+
+Q.Given an array, rotate all elements to the left by one position.
+
+so consedring that all alements have the position of right 
+
+and changing the position of all elements 
+
+
+Store the first element.
+
+Shift every element one position to the left.
+
+Put the stored first element at the last index.
+
+"""
+
+
+def left_rotate(arr):
+    if len(arr) == 0:
+        return arr
+
+    temp = arr[0]
+
+    for i in range(len(arr) - 1):
+        arr[i] = arr[i + 1]
+
+    arr[-1] = temp
+    return arr
+
+
+arr = [10, 20, 30, 40, 50]
+
+print(left_rotate(arr))

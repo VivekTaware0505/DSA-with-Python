4654d58851302fb8e08c582f2bbeb1ab9f07a58b
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-08 21:15:24 +0530

    can we rotate without extra space

--- Arrays3.py
+++ Arrays3.py
@@ -34,3 +34,40 @@ def left_rotate(arr):
 arr = [10, 20, 30, 40, 50]
 
 print(left_rotate(arr))
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+"""
+Q2 Can we rotate without extra space?
+
+we need only one temporary variable 
+
+The array contains only one element.
+
+temp = 1
+
+there are no other element to shift 
+place temp back at the last position which is the same position
+
+so array remains the unchanged 
+
+
+"""
+
+def left_rotate(arr):
+    if len(arr) == 0:
+        return arr
+
+    temp = arr[0]
+
+    for i in range(len(arr) - 1):
+        arr[i] = arr[i + 1]
+
+    arr[-1] = temp
+
+    return arr
+
+
+arr = [1]
+
+print(left_rotate(arr))

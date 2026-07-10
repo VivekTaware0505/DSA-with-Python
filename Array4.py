18810752f87852c06d6218369f3a0a250aace26e
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-10 14:17:04 +0530

    Given an unsorted array, find the length of the longest consecutive sequence.

--- Array4.py
+++ Array4.py
@@ -39,3 +39,47 @@ def next_permutation(nums):
 
 arr = [1, 2, 3]
 print(next_permutation(arr))
+
+
+
+
+"""
+
+Q.2 Given an unsorted array, find the length of the longest consecutive sequence.
+[100, 4, 200, 1, 3, 2]
+
+A consecutive sequence means numbers that follow each other continuously.
+
+for every number
+
+Search for the next number (x+1)
+Then search for (x+2)
+Continue until the sequence breaks.
+
+
+"""
+
+def longest_consecutive(nums):
+
+    num_set = set(nums)
+    longest = 0
+
+    for num in num_set:
+
+        if num - 1 not in num_set:
+
+            current = num
+            length = 1
+
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+
+            longest = max(longest, length)
+
+    return longest
+
+
+arr = [100,4,200,1,3,2]
+
+print(longest_consecutive(arr))

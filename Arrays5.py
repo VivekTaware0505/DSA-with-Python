11d201b3b9e5fc44b8aac3a0fa81c11628219b8b
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-12 16:36:09 +0530

    intersection of two sorted arrays

--- Arrays5.py
+++ Arrays5.py
@@ -2,7 +2,7 @@
 
 Union of two sorted Arrays
 
-Q given two sorted arrays return the union of both arays 
+Q given two sorted arrays return the union of both arrays 
 
 the union should
 
@@ -66,3 +66,43 @@ arr1 = [1,2,3,4,5]
 arr2 = [2,3,4,4,5,6]
 
 print(union(arr1, arr2))
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+Intersection of two sorted arrays 
+
+Q. Given two sorted arrays return their intersection
+the intersection contains only hte common elements
+if an element appears multiple times include it only as many as it appears in both arrays
+
+
+
+"""
+
+def intersection(arr1, arr2):
+    i = 0
+    j = 0
+    ans = []
+
+    while i < len(arr1) and j < len(arr2):
+
+        if arr1[i] < arr2[j]:
+            i += 1
+
+        elif arr1[i] > arr2[j]:
+            j += 1
+
+        else:
+            ans.append(arr1[i])
+            i += 1
+            j += 1
+
+    return ans
+
+
+arr1 = [1,2,2,3,4,5]
+arr2 = [2,2,3,5,6]
+
+print(intersection(arr1, arr2))

bfed01ba4fddbb26aebf5fca047c75f8b0b72dec
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-17 14:20:20 +0530

    Spiral matrix

--- Arrays6.py
+++ Arrays6.py
@@ -33,3 +33,64 @@ matrix = [
 ]
 
 print(rotate(matrix))
+
+
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+Spiral Matrix 
+
+
+Q.1 given an M x N matrix print all the elements in spiral order 
+
+
+
+"""
+def spiralOrder(matrix):
+
+    result = []
+
+    if not matrix:
+        return result
+
+    top = 0
+    bottom = len(matrix) - 1
+    left = 0
+    right = len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+
+        # Left → Right
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        # Top → Bottom
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        # Right → Left
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+
+        # Bottom → Top
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+    return result
+
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(spiralOrder(matrix))

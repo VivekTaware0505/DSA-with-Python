d36968a984624c5717e4cd67b8b7d85e25e138ba
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-17 21:09:18 +0530

    checking

--- Arrays6.py
+++ Arrays6.py
@@ -94,3 +94,32 @@ matrix = [
 ]
 
 print(spiralOrder(matrix))
+
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+"""
+Pascals Triangle 
+
+generate the first N rows of pascals triangle 
+
+
+
+
+"""
+
+def generate(numRows):
+    triangle = []
+
+    for i in range(numRows):
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+        triangle.append(row)
+
+    return triangle
+
+
+print(generate(5))

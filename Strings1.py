f3ca50d383b8acaa50fda59cc1d93ad79e60a838
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-22 14:53:04 +0530

    visual representation of string and indexing

---
+++ Strings1.py
@@ -0,0 +1,24 @@
+"""
+Memory representation:
+
++---+---+---+---+---+---+
+| P | y | t | h | o | n |
++---+---+---+---+---+---+
+
+Visual Representation
+Word = " python"
+"""
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+String Indexing 
+python index start from 0 
+
+"""
+word = " python"
+print(word[0])
+print(word[1])
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")

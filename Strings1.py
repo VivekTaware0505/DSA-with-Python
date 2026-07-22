34b2cb7ac0c495f7f62400d3d7f1bd6713becb10
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-22 15:10:24 +0530

    reverse index and loops

--- Strings1.py
+++ Strings1.py
@@ -22,3 +22,37 @@ print(word[0])
 print(word[1])
 
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+"""
+Negative / or reverse  indexing 
+Python also supports negative indexing.
+
+ P   y   t   h   o   n
+-6  -5  -4  -3  -2  -1
+
+"""
+
+print(word[-1])
+print(word[-3])
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+"""
+after creating a string you cannot change indivisual characters
+
+python creates a new string instead of mofdifying the old one 
+
+name = " Vivek"
+name[0] = "S"
+"""
+
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+"loop in python  "
+
+
+for ch in "Python":
+    print(ch)
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+print("------------------------------Vivek Learning DSA Python----------------------------------------")

ec31c0b3faf1c7ecb7286c8dd8bdfb45a8cb43f9
VIVEK TAWARE <vivektaware16@gmail.com>
2026-07-30 14:25:54 +0530

    longest palindromic substring

--- String/String4.py
+++ String/String4.py
@@ -134,7 +134,142 @@ print(longest_string("abcabcbb"))
 
 
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+
+Longest palindromic substring
+
+
+What is a Palindrome?
+
+A palindrome is a string that reads the same from left to right and right to left.
+
+
+What is Longest Palindromic Substring?
+
+Find the longest continuous substring that is a palindrome.
+
+Example 1
+Input:
+babad
+
+Output:
+bab
+
+Note: "aba" is also a correct answer.
+
+
+Idea
+Generate every possible substring.
+Check if it is a palindrome.
+Keep the longest one.
+
+"""
+def is_palindrome(s):
+    return s == s[::-1]
+
+def longest_palindrome(s):
+
+    answer = ""
+
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+
+            temp = s[i:j+1]
+
+            if is_palindrome(temp):
+
+                if len(temp) > len(answer):
+                    answer = temp
+
+    return answer
+
+print(longest_palindrome("babad"))
+
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+Every palindrome has a center.
+
+There are two types:
+
+
+
+
+
+"""
+
+def expand(s, left, right):
+
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+
+        left -= 1
+        right += 1
+
+    return s[left+1:right]
+
+
+def longest_palindrome(s):
+
+    answer = ""
+
+    for i in range(len(s)):
+
+        # Odd length palindrome
+        p1 = expand(s, i, i)
+
+        # Even length palindrome
+        p2 = expand(s, i, i+1)
+
+        if len(p1) > len(answer):
+            answer = p1
+
+        if len(p2) > len(answer):
+            answer = p2
+
+    return answer
+
+
+print(longest_palindrome("babad"))
+
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+
+"""
+
+count all palindromic substrings
+
+"""
+def count_palindromes(s):
+
+    count = 0
+
+    def expand(left, right):
+        nonlocal count
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+
+            count += 1
+            left -= 1
+            right += 1
+
+    for i in range(len(s)):
+
+        expand(i, i)
+        expand(i, i+1)
+
+    return count
+
+print(count_palindromes("aaa"))
+
+
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
 
 
 

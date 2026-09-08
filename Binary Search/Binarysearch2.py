8bbdce88af0536c6ea1ec49dd40c83420b0a88d7
VIVEK TAWARE <vivektaware16@gmail.com>
2026-09-08 20:14:05 +0530

    example of floor and celi

--- Binary Search/Binarysearch2.py
+++ Binary Search/Binarysearch2.py
@@ -270,6 +270,102 @@ print(find_floor(arr, 6))
 
 
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+"""
+
+Ceil Using Binary Search
+Now we need:
+smallest element >= target
+If:
+arr[mid] >= target
+we found a possible Ceil.
+Save it:
+answer = arr[mid]
+But maybe there is a smaller valid value.
+So go left:
+high = mid - 1
+Otherwise:
+arr[mid] < target
+The value is too small.
+Go right:
+low = mid + 1
+
+
+"""
+
+
+def find_ceil(arr, target):
+
+    low = 0
+    high = len(arr) - 1
+    answer = None
+
+    while low <= high:
+
+        mid = low + (high - low) // 2
+
+        if arr[mid] >= target:
+            answer = arr[mid]
+            high = mid - 1
+
+        else:
+            low = mid + 1
+
+    return answer
+
+
+arr = [1, 3, 5, 7, 9]
+
+print(find_ceil(arr, 6))
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
+
+
+
+
+"""
+
+This is the Version i'd recommend you understand well for interviews 
+
+"""
+
+
+
+def floor_ceil(arr, target):
+
+    low = 0
+    high = len(arr) - 1
+
+    floor = None
+    ceil = None
+
+    while low <= high:
+
+        mid = low + (high - low) // 2
+
+        if arr[mid] == target:
+            return arr[mid], arr[mid]
+
+        elif arr[mid] < target:
+            floor = arr[mid]
+            low = mid + 1
+
+        else:
+            ceil = arr[mid]
+            high = mid - 1
+
+    return floor, ceil
+
+
+arr = [1, 3, 5, 7, 9]
+
+floor, ceil = floor_ceil(arr, 6)
+
+print("Floor:", floor)
+print("Ceil:", ceil)
+
+
+
+
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
 print("------------------------------Vivek Learning DSA Python----------------------------------------")
